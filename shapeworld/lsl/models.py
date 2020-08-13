ea4a292cbbb2010eb0a614571644175c9060aa47
slenderham <wc199951@gmail.com>
2020-08-12 23:53:36 -0500

    try more

--- shapeworld/lsl/models.py
+++ shapeworld/lsl/models.py
@@ -1003,17 +1003,13 @@ class TransformerScorer(Scorer):
         super(TransformerScorer, self).__init__();
         encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_size, nhead=4, dim_feedforward=2*hidden_size, dropout=0.0);
         self.model = nn.TransformerEncoder(encoder_layer, num_layers=2);
-        self.support_emb = nn.Parameter(torch.randn(1, 1, hidden_size)*(hidden_size**0.5));
-        self.query_emb = nn.Parameter(torch.randn(1, 1, hidden_size)*(hidden_size**0.5));
-        self.scorer = nn.Linear(hidden_size, 1);
+        self.agg = nn.Parameter(torch.randn(1, 1, hidden_size))
 
     def score(self, support, query):
         b, n_ex, num_slots, h = support.shape;
-        support = support.flatten(1, 2);
-        support = support + self.support_emb;
-        query = query + self.query_emb;
-        total_in = torch.cat([support, query], dim=1).transpose(0, 1);
-        out = self.model(total_in);
-        support = out[:n_ex*num_slots].mean(0)
-        query = out[n_ex*num_slots:].mean(0)
-        return torch.sum(support * query, dim=1);
+        support = support.flatten(0, 1).transpose(0, 1);
+        support_out = self.model(torch.cat([self.agg.reshape(1, b*n_ex, h), support]));
+        support_out = support_out[0,...].reshape(b, n_ex, h).mean(1);
+        query_out = self.model(torch.cat([self.agg.reshape(1, b, h), query]));
+        query_out = query_out[0,...]
+        return torch.sum(support_out * query_out, dim=1);

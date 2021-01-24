a0ee88bbc108e620de21d5d636dcee8952558acd
slenderham <wc199951@gmail.com>
2021-01-24 01:59:59 -0600

    bug fix

--- shapeworld/lsl/models.py
+++ shapeworld/lsl/models.py
@@ -206,7 +206,7 @@ class RelationalSlotAttention(nn.Module):
         self.to_v = nn.Linear(dim, dim, bias=False)
 
         self.obj_gru = nn.GRUCell(dim*2, dim)
-        self.rel_gru = nn.GRUCell(dim*2, dim)
+        self.rel_gru = nn.GRUCell(dim*4, dim)
 
         self.obj_mlp = nn.Sequential(
             nn.Linear(dim, hidden_dim),
@@ -235,8 +235,8 @@ class RelationalSlotAttention(nn.Module):
         x_i = x_i.expand(b, n_s, n_s, h).flatten(1, 2)  # b. n_s*n_s, h: x1x1x1...x2x2x2...x3x3x3...
         x_j = torch.unsqueeze(x, 1)  # b, 1, n_s, h
         x_j = x_j.expand(b, n_s, n_s, h).flatten(1, 2)  # b. n_s*n_s, h: x1x2x3...x1x2x3...x1x2x3...
-        rel_msg = torch.cat([x_i, x_j], dim=-1)
-        assert(rel_msg.shape==(b, n_s*n_s, 2*h)), f"x_rel's shape is {rel_msg.shape}"
+        rel_msg = torch.cat([x_i, x_j, x_i-x_j, x_i*x_j], dim=-1)
+        assert(rel_msg.shape==(b, n_s*n_s, 4*h)), f"x_rel's shape is {rel_msg.shape}"
         return rel_msg
 
     def _rel_to_obj(self, x_rel, x_obj):
@@ -300,7 +300,7 @@ class RelationalSlotAttention(nn.Module):
             edge_context_w_diag = self._obj_to_rel(obj_slots)
             edge_context = edge_context_w_diag[:, self.non_diag_idx, :]
             rel_slots = self.rel_gru(
-                edge_context.reshape(b*n_s*(n_s-1), 2*d),
+                edge_context.reshape(b*n_s*(n_s-1), 4*d),
                 rel_slots.reshape(b*n_s*(n_s-1), d)
               ).reshape(b, n_s*(n_s-1), d)
             rel_slots = rel_slots + self.rel_mlp(self.norm_pre_ff_rel(rel_slots))
@@ -1163,7 +1163,7 @@ class BilinearScorer(DotPScorer):
         return super(BilinearScorer, self).batchwise_score(x, wy)
 
 class SinkhornScorer(Scorer):
-    def __init__(self, hidden_dim=None, iters=20, reg=0.1, cross_domain_weight=0.5, comparison='im_lang', im_blocks=[6, 30], im_dustbin=None, **kwargs):
+    def __init__(self, hidden_dim=None, iters=10, reg=0.1, cross_domain_weight=0.5, comparison='im_lang', im_blocks=[6, 30], im_dustbin=None, **kwargs):
         super(SinkhornScorer, self).__init__()
         assert(comparison in ['eval', 'im_im', 'im_lang'])
         self.cross_domain_weight = cross_domain_weight
@@ -1358,10 +1358,10 @@ class SinkhornScorer(Scorer):
         else:
             log_mu = -ms.log().reshape(1, 1).expand(b, m)
             log_nu = -ns.log().reshape(1, 1).expand(b, n)
-        
+
         if (scores_mask is not None):
             log_nu = log_nu.masked_fill(scores_mask[:, 0, :], mask_val)
-        
+
         if use_ipot:
             Z = self.log_ipot(couplings, log_mu, log_nu, scores_mask, iters)
         else:

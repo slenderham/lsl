a35fa29695eaca9356b02ec7e6b1f667bcc074a6
slenderham <wc199951@gmail.com>
2021-01-24 21:37:41 -0600

    bug fix

--- shapeworld/lsl/utils.py
+++ shapeworld/lsl/utils.py
@@ -54,13 +54,14 @@ class AverageMeter(object):
 
 
 def save_checkpoint(state, is_best, folder='./',
-                    filename='checkpoint.pth.tar'):
+                    filename='checkpoint.pth.tar', 
+                    best_filename='model_best.pth.tar'):
     if not os.path.isdir(folder):
         os.mkdir(folder)
     torch.save(state, os.path.join(folder, filename))
     if is_best:
         shutil.copyfile(os.path.join(folder, filename),
-                        os.path.join(folder, 'model_best.pth.tar'))
+                        os.path.join(folder, best_filename))
 
 
 def merge_args_with_dict(args, dic):
